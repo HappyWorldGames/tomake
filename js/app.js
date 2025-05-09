e660bc2a6d621d37d3932d1ec3313f23cfeadab9
HappyWorld <xeler.arsk@gmail.com>
2025-05-09 23:06:49 +0300

    fix

--- js/app.js
+++ js/app.js
@@ -206,6 +206,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 		
 		// Обновляем из облака при наличии более новых версий
 		remote.forEach(task => {
+		  alert(task);
 		  if (task) {
   			const existing = taskMap.get(task.id);
   			if (!existing || task.lastModified > existing.lastModified) {
